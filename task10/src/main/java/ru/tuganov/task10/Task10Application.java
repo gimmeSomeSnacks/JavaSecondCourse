7a6b90a56e4b9f81229bc9ee4e533b5b5e5d7418
gimmeSomeSnacks <n-tuganov@mail.ru>
2024-03-02 01:00:29 +0300

    second push

---
+++ task10/src/main/java/ru/tuganov/task10/Task10Application.java
@@ -0,0 +1,11 @@
+package ru.tuganov.task10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task10Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Task10Application.class, args);
+	}
+}
