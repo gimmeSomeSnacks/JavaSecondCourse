320450aaf5e0e1a2569062a16475f0e1a9dc8d91
Tuganov Nikita Alexandrovich <139561847+gimmeSomeSnacks@users.noreply.github.com>
2024-03-17 14:35:22 +0300

    Add files via upload

---
+++ task14-1/src/main/java/ru/tuganov/task141/University.java
@@ -0,0 +1,13 @@
+package ru.tuganov.task141;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class University {
+    private String name;
+    private String creation_date;
+}
---
+++ task14-1/src/test/java/ru/tuganov/task141/Task141ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tuganov.task141;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task141ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
