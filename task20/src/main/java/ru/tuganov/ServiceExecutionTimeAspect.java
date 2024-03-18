95546ec787f0619cc8f5e4de2d817852eace1bfc
Tuganov Nikita Alexandrovich <139561847+gimmeSomeSnacks@users.noreply.github.com>
2024-03-18 21:37:43 +0300

    Add files via upload

---
+++ task20/src/main/java/ru/tuganov/ServiceExecutionTimeAspect.java
@@ -0,0 +1,26 @@
+package ru.tuganov;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ServiceExecutionTimeAspect {
+    private long startTime;
+
+    @Before("execution(* ru.tuganov.Service.*(..))")
+    public void beforeMethodExecution() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @After("execution(* ru.tuganov.Service.*(..))")
+    public void afterMethodExecution(JoinPoint joinPoint) {
+        long executionTime = System.currentTimeMillis() - startTime;
+        log.info("Метод " + joinPoint.getSignature() + " выполнен за " + executionTime + " мс");
+    }
+}
