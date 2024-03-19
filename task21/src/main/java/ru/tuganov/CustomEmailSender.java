2b47dfa9952e3634c3c5d22bed153b7ecb606d33
Tuganov Nikita Alexandrovich <139561847+gimmeSomeSnacks@users.noreply.github.com>
2024-03-19 16:23:30 +0300

    Add files via upload

---
+++ task21/src/main/java/ru/tuganov/CustomEmailSender.java
@@ -0,0 +1,23 @@
+package ru.tuganov;
+
+import lombok.AllArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import ru.tuganov.entities.Subject;
+
+@org.springframework.stereotype.Service
+@AllArgsConstructor
+public class CustomEmailSender{
+    private final JavaMailSender javaMailSender;
+
+    @Async
+    public void sendEmail(Subject subject) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom("nikita.tuganoff@gmail.com");
+        mailMessage.setTo("n-tuganov@mail.ru");
+        mailMessage.setSubject("Добавлено что-то");
+        mailMessage.setText("Добавлено: " + subject.toString());
+        javaMailSender.send(mailMessage);
+    }
+}
